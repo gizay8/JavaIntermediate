e0dc574f7103a1c52d530a74039193984807e425
gizay8 <91142274+gizay8@users.noreply.github.com>
2021-12-07 20:09:29 +0300

    AdventureGame

--- AdventureGame/src/main/java/Mine.java
+++ AdventureGame/src/main/java/Mine.java
@@ -1,5 +1,4 @@
 public class Mine extends BattleLoc{
-
     public Mine(Player player) {
         super(player, "Maden", new Snake(), "-", 5);
     }
--- AdventureGame/src/main/java/River.java
+++ AdventureGame/src/main/java/River.java
@@ -1,5 +1,4 @@
 public class River extends BattleLoc{
-
     public River(Player player) {
         super(player, "Nehir", new Bear(), "Water", 2);
     }
