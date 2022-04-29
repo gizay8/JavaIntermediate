e557b229b40bc233f9fde32abe42759f971625c1
Gizay Cin <cingizaycin@gmail.com>
2022-04-29 19:56:30 +0300

    PatikaClone

--- PatikaClone/src/com/patikadev/Helper/Main.java
+++ PatikaClone/src/com/patikadev/Helper/Main.java
@@ -4,8 +4,10 @@ package com.patikadev.Helper;
 import com.patikadev.View.LoginGUI;
 
 public class Main {
+
     public static void main(String[] args) {
         LoginGUI login = new LoginGUI();
     }
+    
 }
 
